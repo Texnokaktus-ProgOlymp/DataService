6c6e9de657f6338da69b66b35bd754104e290944
Artem Krylov <kav128@outlook.com>
2025-02-20 14:57:41 +0300

    add telemetry

--- Texnokaktus.ProgOlymp.Data/Program.cs
+++ Texnokaktus.ProgOlymp.Data/Program.cs
@@ -1,4 +1,6 @@
+using Serilog;
 using Texnokaktus.ProgOlymp.Data.Infrastructure;
+using Texnokaktus.ProgOlymp.OpenTelemetry;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,13 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 
 builder.Services.AddGrpcClients(builder.Configuration);
 
+builder.Services.AddTexnokaktusOpenTelemetry(builder.Configuration, "DataService", null, null);
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 if (!app.Environment.IsDevelopment())
 {
@@ -25,4 +33,4 @@ app.MapStaticAssets();
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}")
    .WithStaticAssets();
 
-app.Run();
+await app.RunAsync();
